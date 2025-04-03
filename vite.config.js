2ee982af68f45477c0a3cfc5e85e1e53ca2b1e98
joeiradionela <joeiradionela@gmail.com>
2025-04-04 01:53:46 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pit3_frontend/',
+  base: '/pit3_todolist_frontend/',
 })
